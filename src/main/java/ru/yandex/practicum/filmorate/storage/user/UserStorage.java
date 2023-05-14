86f63f79989ca3a8ef183f5f94da3dab26ab48c8
lev7ne <ros.kh.23@yandex.ru>
2023-05-14 23:40:29 +0300

    правки checkstyle4

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,12 +1,13 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
 
 public interface UserStorage {
     User saveUser(User user);
+
     Collection<User> getUsers();
+
     public User getUserById(Integer id);
 }
