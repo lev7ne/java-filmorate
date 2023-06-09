1dd0a43dd4727bddefe49b8b6dbae939bb90d792
Rostislav <ros.kh.23@yandex.ru>
2023-06-09 18:55:58 +0300

    тз11 рефакторинг v3 (+замечания ревьюера)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
-INSERT INTO genres (genre_name)
-VALUES ('Комедия'),
-       ('Драма'),
-       ('Мультфильм'),
-       ('Триллер'),
-       ('Документальный'),
-       ('Боевик');
+MERGE INTO genres (GENRE_ID, GENRE_NAME)
+    VALUES (1, 'Комедия'),
+           (2, 'Драма'),
+           (3, 'Мультфильм'),
+           (4, 'Триллер'),
+           (5, 'Документальный'),
+           (6, 'Боевик');
 
-INSERT INTO ratings (RATING_NAME)
-VALUES ('G'),
-       ('PG'),
-       ('PG-13'),
-       ('R'),
-       ('NC-17');
+MERGE INTO ratings (RATING_ID, RATING_NAME)
+    VALUES (1, 'G'),
+           (2, 'PG'),
+           (3, 'PG-13'),
+           (4, 'R'),
+           (5, 'NC-17');
